d02eb8a8a1e7ab73296ec5be18a459d941d0a10a
Axel Vallon <axel.vallon123@gmail.com>
2022-01-22 16:03:25 +0100

    change dynamic content

--- demo/backend/index.js
+++ demo/backend/index.js
@@ -3,10 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(makeid(10))
 })
 
 app.use(express.static('public'));
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
